70af4ce79d81d39404af9efe3849ea1a2db5e21b
FabioFiorita <fabiolfp@gmail.com>
2021-08-11 10:31:19 -0300

    Initial Commit

---
+++ meuMedicamento/meuMedicamentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  meuMedicamentoApp.swift
+//  meuMedicamento
+//
+//  Created by Fabio Fiorita on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct meuMedicamentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
