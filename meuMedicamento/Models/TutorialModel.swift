d2d8d0e669952e95ed5605e2a45adc2e7443035f
Fabio Fiorita <fabiolfp@gmail.com>
2021-11-23 14:28:07 -0300

    new app logo

--- meuMedicamento/Models/TutorialModel.swift
+++ meuMedicamento/Models/TutorialModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 let tabs = [
-    Page(image: "Logo", title: "Organize seus medicamentos", text: "meuMedicamento tem como objetivo ajudar pessoas a controlar seus medicamentos, por meio dele o usuário consegue receber notificações, alertas se o medicamento estiver acabando e é preciso comprar mais e visualizar um mapa com todas as farmácias e hospitais próximos da sua localização."),
+    Page(image: "Logo SF", title: "Organize seus medicamentos", text: "meuMedicamento tem como objetivo ajudar pessoas a controlar seus medicamentos, por meio dele o usuário consegue receber notificações, alertas se o medicamento estiver acabando e é preciso comprar mais e visualizar um mapa com todas as farmácias e hospitais próximos da sua localização."),
     Page(image: "pill", title: "Controle seus medicamentos", text: "Adicione seus medicamentos utilizando o botão + no canto superior esquerdo do app\nTenha controle da quantidade de medicamentos restantes, horários, histórico de uso e muito mais."),
     Page(image: "map", title: "Descubra as farmácias e hospitais próximas", text: "Utilize o mapa que está dentro do app para achar todas as farmácias e hospitais próximos da sua localização"),
     Page(image: "gear", title: "Personalize seu uso", text: "Configure funções do app ou contate o suporte.")
